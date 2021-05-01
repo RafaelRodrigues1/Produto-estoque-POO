b2b0db4bd4deb6a58120f3de8ef8f91ebd152eb9
RafaelRodrigues1 <rafaelx7t@hotmail.com>
2021-05-01 13:51:59 -0300

    Alterações em textos

--- src/produtoestoque/Produto.java
+++ src/produtoestoque/Produto.java
@@ -7,49 +7,48 @@ import java.util.Scanner;
  */
 public class Produto {
     
-    
+            //Atributos da classe
     Scanner in = new Scanner(System.in);
     private String nome;
     private double preco;
     private int quantidade;
-
+            
+            //Método construtor
     public Produto(String nome, double preco, int quant){
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quant;
     }
-    
+
+            //Métodos getters
     public String getNome(){
         return this.nome;
-    }
-    
+    }    
     public double getPreco(){
         return this.preco;
-    }
-    
+    }    
     public int getQuantidade(){
         return this.quantidade;
     }
-    
+
+            //Método set
     public void setQuantidade(int quantidade){
         this.quantidade = quantidade;
     }
-    
+
+            //Outros métodos
     private Double calcPrecoTotal(){
         double precototal = getPreco()*getQuantidade();
         return precototal;
-    }
-    
+    }    
     public void addProduto(){
         System.out.print("Quantos produtos deseja adicionar ao estoque? ");
         this.quantidade += in.nextInt();
-    }
-    
+    }    
     public void removeProduto(){
         System.out.print("Quantos produtos deseja remover do estoque? ");
         this.quantidade -= in.nextInt();
-    }
-    
+    }    
     public void infoProduto(){
         System.out.printf("Nome do produto: \t%s\n", getNome());
         System.out.printf("Preço do produto: \tR$%.2f\n", getPreco());
--- src/produtoestoque/ProdutoEstoque.java
+++ src/produtoestoque/ProdutoEstoque.java
@@ -13,21 +13,23 @@ public class ProdutoEstoque {
         String p = new String();
         System.out.print("Quantos produtos deseja cadastrar? ");
         int a = in.nextInt();
+        in.nextLine();
         Produto produto[] = new Produto[a];
         for(int i = 0; i<produto.length; i++){            
             System.out.printf("(CADASTRO) Digite o nome do produto%d: ", i+1);
-            String nome = in.next();
-            System.out.printf("(CADASTRO) Digite o preço do produto%d: ", i+1);
+            String nome = in.nextLine();
+            System.out.printf("(CADASTRO) Digite o preço do produto%d: R$", i+1);
             double preco = in.nextDouble();
             System.out.printf("(CADASTRO) Digite a quantidade de entrada do produto%d: ", i+1);
-            int quant = in.nextInt();        
+            int quant = in.nextInt();
+            in.nextLine();
             produto[i] = new Produto(nome, preco, quant);
             }
         
         do{
             System.out.println("----------------------------------------------");
             System.out.print("Digite nome do produto (0 PARA SAIR): ");
-            p = in.next().toUpperCase();
+            p = in.nextLine().toUpperCase();
             for (Produto pesquisa : produto) {
                 if (p.equals(pesquisa.getNome().toUpperCase())){
                     int resp;
@@ -35,16 +37,18 @@ public class ProdutoEstoque {
                         System.out.print(pesquisa.getNome().toUpperCase() + ": ADICIONAR[1], REMOVER[2] "
                                 + "ou CONSULTAR[3]([0] para SAIR)");
                         resp = in.nextInt();
+                        in.nextLine();
                         switch(resp){
                             case 1 -> pesquisa.addProduto();
                             case 2 -> pesquisa.removeProduto();
                             case 3 -> pesquisa.infoProduto();
-                            case 0 -> System.out.println("Fim de programa!");
+                            case 0 -> System.out.println("Saindo do menu do produto " + pesquisa.getNome().toUpperCase());
                             default -> System.out.println("DIGITE UMA OPÇÃO VÁLIDA!");
                         }
                     }while(resp!=0);
-                }                         
+                }                       
             }
         }while(!(produto.equals("0")));
+        System.out.println("Fim de programa!"); 
     }    
 }
